ebf88dd1852fb6dfafc7abf420c7d14bf2938e37
AlexisJcr <alexisjoncour@free.fr>
2024-11-30 22:21:05 +0100

    edit cp quests

--- app/quest/page.tsx
+++ app/quest/page.tsx
@@ -1,13 +1,42 @@
-import QuestComponent from "@/ui/components/quest/Quest"
-import VercelIcon from "@/public/vercel.svg"
-
+import QuestComponent from "@/ui/components/quest/Quest";
+import VercelIcon from "@/public/vercel.svg";
 
 export default function AboutPage() {
   return (
-    <div className="bg-slate-800/50 rounded-xl p-4 space-y-6">
-      <h1>Composant Quest</h1>
-      <QuestComponent text="Gagne un big bird duolingo" color="#ffffff" current={16} max={30} iconQuest={VercelIcon} iconBar={VercelIcon}></QuestComponent>
-
+    <div className="bg-primary p-4">
+      <h1 className="text-xl mb-5 font-semibold">Quêtes du jour</h1>
+      <div className="bg-tertiary p-4 rounded-xl">
+        <QuestComponent
+          id={1}
+          className=""
+          icon={VercelIcon}
+          text="Finir le composant"
+          color="#b61818"
+          reward={50}
+          current={16}
+          max={20}
+        ></QuestComponent>
+        <QuestComponent
+          id={2}
+          className=""
+          icon={VercelIcon}
+          text="Acheter 10 big birds"
+          color="#1871b6"
+          reward={200}
+          current={3}
+          max={10}
+        ></QuestComponent>
+        <QuestComponent
+          id={3}
+          className=""
+          icon={VercelIcon}
+          text="Insulter Pouce Bleu"
+          color="#18b629"
+          reward={1000}
+          current={100}
+          max={100}
+        ></QuestComponent>
+      </div>
     </div>
   );
 }
--- ui/components/quest/Quest.tsx
+++ ui/components/quest/Quest.tsx
@@ -1,65 +1,80 @@
 import clsx from "clsx";
 import Image from "next/image";
 
+import defaultIcon from "@/public/next.svg";
+
 interface Props {
   /* Props Case globale */
+  id: number;
   className?: string;
-  text?: string;
+  text: string;
+  reward: number;
+  current: number;
+  max: number;
+
   color?: string;
-  iconQuest?: any;
-  /* Props Barre de progression */
-  iconBar?: any;
-  current?: number;
-  max?: number;
+  icon?: any;
 }
 
-const ProgressBar = ({ current, max, color }: { current: number; max: number; color: string }) => {
-    const percentage = Math.min(Math.max(current, 0), max) / max * 100;
-  
-    return (
-      <div className="relative h-2 w-full overflow-hidden rounded-full bg-slate-700">
-        <div
-          className="absolute left-0 top-0 h-full transition-all"
-          style={{ width: `${percentage}%`, backgroundColor: color }}
-        />
-      </div>
-    );
+const ProgressBar = ({
+  current,
+  max,
+  color,
+}: {
+  current: number;
+  max: number;
+  color: string;
+}) => {
+  const percentage = (Math.min(Math.max(current, 0), max) / max) * 100;
+
+  return (
+    <div className="relative h-4 w-full overflow-hidden rounded-full bg-secondary">
+      <div
+        className="absolute left-0 top-0 h-full transition-all rounded-full"
+        style={{ width: `${percentage}%`, backgroundColor: color }}
+      />
+    </div>
+  );
 };
 
 export const Quest = ({
-    className,
-    text,
-    color = "#ffffff",
-    iconQuest,
-    iconBar,
-    current = 0,
-    max = 10,
-  }: Props) => {
-    return (
-      <div className={clsx("space-y-2", className)}>
-        <div className="flex items-center gap-3">
-          {/* Icone de Quête */}
-          {iconQuest && (
-            <Image src={iconQuest} alt="iconQuest" width={32} height={32} />
-          )}
-          {/* Texte */}
-          <div className="text-white font-medium">{text}</div>
+  id,
+  className,
+  text,
+  color = "#ffffff",
+  icon = { defaultIcon },
+  reward,
+  current,
+  max,
+}: Props) => {
+
+  return (
+    <div className={clsx("space-y-2 p-4")}>
+      <div className="flex items-center gap-3">
+        {/* Icon */}
+        <Image src={icon} alt="iconQuest" width={32} height={32} />
+        {/* Texte */}
+        <div className="text-white font-medium">Quête n°{id} :</div>
+        <div className="text-white font-normal">{text}</div>
+      </div>
+      {/* Barre de progression */}
+      <div className="relative">
+        <ProgressBar current={current} max={max} color={color} />
+        <div className="absolute right-0 top-1/2 -translate-y-1/2">
+          <div className={`badge ${current === max ? "bg-green-600" : "bg-gray-600"} text-white text-sm rounded-sm px-3 mt-3 py-1`}>
+            {current === max ? "🎉 Quête Terminée" : "Quête en cours"}
+          </div>
         </div>
-        {/* Barre de progression */}
-        <div className="relative">
-          <ProgressBar current={current} max={max} color={color} />
-          {iconBar && (
-            <div className="absolute right-0 top-1/2 -translate-y-1/2">
-              <div className="rounded-lg p-1">
-                <Image src={iconBar} alt="iconBar" width={16} height={16} />
-              </div>
-            </div>
-          )}
-          {/* Infos sur la progression */}
-          <div className="text-slate-400 text-sm mt-1">{current}/{max}</div>
+        {/* Infos sur la progression */}
+        <div className="text-slate-300 text-sm mt-2">
+          {current}/{max}
+        </div>
+        <div className="text-slate-200 font-medium text-sm mt-2">
+          Récompense : {reward} XP
         </div>
       </div>
-    );
-  };
-  
-  export default Quest;
+    </div>
+  );
+};
+
+export default Quest;
