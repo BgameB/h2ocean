89d27fec9282e518627b045e7883222cd65cac3c
RomainCabaret <123275998+RomainCabaret@users.noreply.github.com>
2024-12-01 19:14:27 +0100

    fix: add quiz test

--- app/lesson/page.tsx
+++ app/lesson/page.tsx
@@ -1,4 +1,32 @@
+import QIncorrect from "@/ui/components/quiz/QIncorrect";
+import Quiz from "@/ui/components/quiz/Quiz";
+
+import VercelIcon from "@/public/vercel.svg";
+
 export default function TeamPage() {
   // SYSTEME DE LECON EN FONCTION DE LA PROGRESSION
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <Quiz
+        id={1}
+        image={VercelIcon}
+        answer1="Carré"
+        answer2="Rectangle"
+        answer3="Triangle"
+        correctAnswer="Triangle"
+        color="#0F121E"
+      />
+
+      <QIncorrect
+        question="Quelles sont les deux plus grandes planètes du système solaire ?"
+        answer1="Mars"
+        answer2="Jupiter"
+        answer3="Saturne"
+        answer4="Vénus"
+        correctAnswer1="Jupiter"
+        correctAnswer2="Saturne"
+        color="#0F121E"
+      />
+    </div>
+  );
 }
