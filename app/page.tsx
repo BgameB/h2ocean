3ea0a53ab1b1972ea34f941b1b52811356faa3df
RomainCabaret <123275998+RomainCabaret@users.noreply.github.com>
2024-12-01 13:20:04 +0100

    fix: resolve small bug in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 // import Image from "next/image";
-import Quest from "@/ui/components/quest/Quest";
 import { Button } from "@/ui/design-system/button/Button";
 
 export default function Home() {
@@ -7,7 +6,6 @@ export default function Home() {
     <div className="flex">
       <nav>
         <Button>DuaoDingo</Button>
-        <Quest current={10} max={15} text="&aa" />
       </nav>
     </div>
   );
