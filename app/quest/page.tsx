db0c968b701a610c09715b3b1c3bd395809f1055
AlexisJcr <alexisjoncour@free.fr>
2024-12-02 20:31:46 +0100

    feat : edit player profile

--- app/quest/page.tsx
+++ app/quest/page.tsx
@@ -6,44 +6,43 @@ import PlayerProfileQuests from "@/ui/components/playerProfile/PlayerProfileQues
 export default function QuestsPage() {
   return (
     <div>
-      <PlayerProfileQuests
-      level={9}
-      maxLevel={15}
-      xp={1500}
-
-      />
-      <h1 className="text-xl mb-5 font-semibold">Quêtes du jour</h1>
-      <div className="bg-tertiary p-4 rounded-xl border-2 border-[#37464F]">
-        <QuestComponent
-          id={1}
-          className="border-b-2 border-[#28333a]"
-          icon={VercelIcon}
-          text="Finir le composant"
-          color="#b61818"
-          reward={50}
-          current={20}
-          max={20}
-        />
-        <QuestComponent
-          id={2}
-          className="border-b-2 border-[#28333a]"
-          icon={VercelIcon}
-          text="Acheter 10 big birds"
-          color="#1871b6"
-          reward={200}
-          current={3}
-          max={10}
-        />
-        <QuestComponent
-          id={3}
-          className=""
-          icon={VercelIcon}
-          text="Finir le jeu"
-          color="#18b629"
-          reward={1000}
-          current={29}
-          max={100}
-        />
+      <h1 className="text-3xl mb-5 font-semibold">Quêtes</h1>
+      <div className="flex justify-center gap-[40px] max-md:mb-[100px]">
+        <div className="bg-tertiary p-4 rounded-xl border-2 border-[#37464F]">
+          <QuestComponent
+            id={1}
+            className="border-b-2 border-[#28333a]"
+            icon={VercelIcon}
+            text="Finir le composant"
+            color="#b61818"
+            reward={50}
+            current={20}
+            max={20}
+          />
+          <QuestComponent
+            id={2}
+            className="border-b-2 border-[#28333a]"
+            icon={VercelIcon}
+            text="Acheter 10 big birds"
+            color="#1871b6"
+            reward={200}
+            current={3}
+            max={10}
+          />
+          <QuestComponent
+            id={3}
+            className=""
+            icon={VercelIcon}
+            text="Finir le jeu"
+            color="#18b629"
+            reward={1000}
+            current={29}
+            max={100}
+          />
+        </div>
+        <div className="max-lg:hidden flex flex-col min-w-[450px]">
+          <PlayerProfileQuests level={9} maxLevel={15} xp={1500} />
+        </div>
       </div>
     </div>
   );
--- ui/components/quest/Quest.tsx
+++ ui/components/quest/Quest.tsx
@@ -49,7 +49,7 @@ export const Quest = ({
 }: Props) => {
 
   return (
-    <div className={clsx("space-y-2 p-4", className)}>
+    <div className={clsx("grid grid-cols-2 p-[25px] max-w-[1000px] gap-[20px] max-sm:flex max-sm:flex-col", className)}>
       <div className="flex items-center gap-3">
         {/* Icon */}
         <Image src={icon} alt="iconQuest" width={32} height={32} />
