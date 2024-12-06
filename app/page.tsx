0b502f95a4873bb3501597dc91e32f97317cbbd2
Cabaret <romainc.cabaret@gmail.com>
2024-12-06 06:52:22 +0100

    fix: try to fix this bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { useEffect } from "react";
+
 export default function Home() {
   // window.location.href = "/learn";
+
+  useEffect(() => {
+    window.location.href = "/learn";
+  }, []);
 
   return <div className="flex"></div>;
 }
